16d5b6911f59e36f7e3682fb771d7c310625ca3c
sasing-tet <vencejumar.sasing.20@usjr.edu.ph>
2023-04-10 21:49:16 +0800

    Flutter e-Commerce App UI Design

--- lib/main.dart
+++ lib/main.dart
@@ -21,7 +21,6 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         scaffoldBackgroundColor: Colors.white,
       ),
-      // home: HomePage(),
       routes: {
         "/": (context) => HomePage(),
         "cartPage": (context) => CartPage(),
